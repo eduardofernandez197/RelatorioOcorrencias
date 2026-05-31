837a3fe8579f56d6c13368a35a01394ea0e7069a
eduardofernandez197 <eduardofefe1970@gmail.com>
2026-05-31 11:31:11 -0300

    commit1

---
+++ src/main/java/com/coruja/ocorrencias/Service/OcorrenciaService.java
@@ -0,0 +1,25 @@
+package com.coruja.ocorrencias.Service;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.coruja.ocorrencias.DTO.OcorrenciaDTO;
+
+public class OcorrenciaService {
+    
+
+
+public void salvar (OcorrenciaDTO ocorrenciaDTO){
+ 
+}
+    public void validarFotos (List<MultipartFile> foto){
+for (MultipartFile multipartFile : foto) {
+
+
+
+
+
+        }
+    }   
+}
---
+++ src/test/java/com/coruja/ocorrencias/OcorrenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coruja.ocorrencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcorrenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
