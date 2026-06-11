69914af3ed13c15bee890ffffa32c05c1616576a
eduardofernandez197 <eduardofefe1970@gmail.com>
2026-06-10 21:16:52 -0300

    ECF_Commit1

--- src/main/java/com/coruja/ocorrencias/Service/ValidaOcorrenciaInterface.java
+++
@@ -1,7 +0,0 @@
-package com.coruja.ocorrencias.service;
-
-import com.coruja.ocorrencias.dto.OcorrenciaDTO;
-
-public interface ValidaOcorrenciaInterface {
-     void validar (OcorrenciaDTO ocorrenciaDTO);
-}
